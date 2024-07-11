348fdeef4d696593d6c75754f80f2e2e2dcf5984
Cloud <ruben.as532@gmail.com>
2024-07-11 10:58:56 +0200

    Almostgit add .! 29:30

--- index.php
+++ index.php
@@ -11,6 +11,8 @@ function printFrame(string $text_to_print): void {
     $words = explode(" ",$text_to_print);
     $longest_word_length = getLongestWordLength($words);
     printFrameRow($longest_word_length);
+    printWordRows($words,$longest_word_length);
+    printFrameRow($longest_word_length);
  }
 
 function getLongestWordLength(array $words): int {
@@ -21,12 +23,29 @@ function getLongestWordLength(array $words): int {
     return $size;
 }
 
-function printFrameRow(int $n_columns):void {
+function printFrameRow(int $n_columns): void {
     $range = range(1,$n_columns+BORDER_PADDING);
     foreach($range as $row) {
-        echo "#";
+        echo FRAME_CHAR;
     }
     echo PHP_EOL;
+}
+
+function printWordRows(array $words,int $longest_word_length): void {
+    foreach($words as $word){
+        echo "# ".$word;
+        printRemainingRow(strlen($word),$longest_word_length);
+    }
+}
+
+function printRemainingRow(int $current_word_length, int $longest_word_length): void {
+    $columns_diff = $longest_word_length-$current_word_length;
+    $con = 0;
+    while($con < $columns_diff) {
+        echo " ";
+        ++$con;
+    }
+    echo " ".FRAME_CHAR.PHP_EOL;
 }
 
 
