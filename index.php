0b98a741d4d6f6506219eebb26bf9c32b767276e
Cloud <ruben.as532@gmail.com>
2024-07-11 10:46:58 +0200

    Words count and longest word length

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
 <?php
+
 define('FRAME_CHAR','#');
+define('BORDER_PADDING',4);
 
+//TODO: Remember, we should validate EVERY input entry. Not the main purpose of the exercise.
 $text_to_print = readline();
+printFrame($text_to_print);
 
-var_dump($text_to_print);
+function printFrame(string $text_to_print): void {
+    $words = explode(" ",$text_to_print);
+    $longest_word_length = getLongestWordLength($words);
+ }
+
+function getLongestWordLength(array $words): int {
+    $size = 0;
+    foreach($words as $word) {
+        if(strlen($word) > $size) $size = strlen($word);
+    }
+    return $size;
+}
+
 
 ?>
